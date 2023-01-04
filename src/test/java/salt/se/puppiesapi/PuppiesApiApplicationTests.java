b670cef3967fbf0d0cbc4c3e494b597996711766
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2023-01-04 10:32:37 +0100

    this is now a java-based api

---
+++ src/test/java/salt/se/puppiesapi/PuppiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package salt.se.puppiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
